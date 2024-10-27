6815dd1a7a38e76b1d59afeef691ea6265fb4054
Volodymyr210 <vova.tsapchuk2@gmail.com>
2024-10-27 12:41:24 +0200

    This is dev-2

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return 'Hello, World!'
+def resume():
+    return render_template('resume.html', title="Резюме")
 
 if __name__ == '__main__':
     app.run(debug=True)
