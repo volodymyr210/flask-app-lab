23712c7a1f11751d3fdafc7c238240881016d71c
Volodymyr210 <vova.tsapchuk2@gmail.com>
2024-10-28 21:24:00 +0200

    Your commit message

---
+++ app/views.py
@@ -0,0 +1,22 @@
+from flask import request, redirect, url_for, render_template, abort
+from . import app
+
+@app.route('/')
+def main():
+    return render_template("base.html")
+
+#users
+
+@app.route("/hi/<string:name>")   #/hi/ivan?age=45
+def greetings(name):
+    name = name.upper()
+    age = request.args.get("age", None, int)   
+
+    return render_template("hi.html", 
+                           name=name, age=age)
+
+@app.route("/admin")
+def admin():
+    to_url = url_for("greetings", name="administrator", age=45, _external=True)     # "http://localhost:8080/hi/administrator?age=45"
+    print(to_url)
+    return redirect(to_url)
