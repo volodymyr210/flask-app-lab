19570c93401c325160488d5582eb51c8c4e9e4e1
Volodymyr210 <vova.tsapchuk2@gmail.com>
2024-10-30 22:49:53 +0200

    Updated

--- app/views.py
+++ app/views.py
@@ -5,18 +5,3 @@ from . import app
 def main():
     return render_template("base.html")
 
-#users
-
-@app.route("/hi/<string:name>")   #/hi/ivan?age=45
-def greetings(name):
-    name = name.upper()
-    age = request.args.get("age", None, int)   
-
-    return render_template("hi.html", 
-                           name=name, age=age)
-
-@app.route("/admin")
-def admin():
-    to_url = url_for("greetings", name="administrator", age=45, _external=True)     # "http://localhost:8080/hi/administrator?age=45"
-    print(to_url)
-    return redirect(to_url)
--- tests/test_user_bp.py
+++ tests/test_user_bp.py
@@ -2,7 +2,6 @@ import unittest
 from app import app  # Переконайтеся, що імпортували ваш Flask-додаток
 
 
-
 class FlaskAppTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -12,14 +11,14 @@ class FlaskAppTestCase(unittest.TestCase):
 
     def test_greetings_page(self):
         """Тест маршруту /hi/<name>."""
-        response = self.client.get("/hi/John?age=30")
+        response = self.client.get("users/hi/John?age=30")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"JOHN", response.data)
         self.assertIn(b"30", response.data)
 
     def test_admin_page(self):
         """Тест маршруту /admin, який перенаправляє."""
-        response = self.client.get("/admin", follow_redirects=True)
+        response = self.client.get("users/admin", follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"ADMINISTRATOR", response.data)
         self.assertIn(b"45", response.data)
